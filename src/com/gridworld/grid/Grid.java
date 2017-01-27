d56bced6179b39b52e8c0bbaa3262835cab9c8ac
Esther <eshiman@gmail.com>
2017-01-27 14:55:33 -0500

    Created Grid Class (in java)
    Created Grid Class (in java) to store Grid Array and white and gray
    "definitions"

---
+++ src/com/gridworld/grid/Grid.java
@@ -0,0 +1,32 @@
+/*
+ * Authors: Joseph Kotzker and Esther Shimanovich
+
+Initialized Array of grid_squares that includes cost dictionaries
+ *
+ */
+package com.gridworld.grid;
+
+import java.util.*;
+
+public class Grid {
+	GridSquare[][] GridSquares = new GridSquare[160][120];
+	Object[][] white  = {{"horiz","vert","diagonal"},{0.5, 0.5, Math.pow(2, 0.5)/2}};
+	Object[][] lightGray  = {{"horiz","vert","diagonal"},{1, 1, Math.pow(2, 0.5)}};
+	
+	Grid(){
+		this.GridSquares = new GridSquare[160][120];
+		for(int i = 0; i<160; i++){
+			for(int j = 0; j<120; j++){
+				GridSquare[i][j]= GridSquare(i,j,color.white);
+			}
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return this.GridSquares.toString();
+	}
+		
+	}
+	
+}
