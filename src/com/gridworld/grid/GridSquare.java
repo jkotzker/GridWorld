46a2f0875e5d45fe527836e8d1d358403092a47a
Joseph Kotzker <joeykotzker@gmail.com>
2017-01-27 14:36:28 -0500

    Some Java classes
    GridSquare, Coordinates, Color classes

--- src/com/gridworld/grid/GridSquare.java
+++ src/com/gridworld/grid/GridSquare.java
@@ -1,7 +1,23 @@
 package com.gridworld.grid;
 
+import com.gridworld.grid.Color;
+
 public class GridSquare {
 	
+	Coordinates coordinates = new Coordinates(0,0);
 	
+	Boolean memberOfVerticalHighway = false;
+	Boolean memberOfHorizontalHighway = false;
+	
+	Color color;
+	
+	public GridSquare(int x, int y, Color color){
+		
+		this.coordinates.setX(x);
+		this.coordinates.setY(y);
+		
+		this.color = color;
+		
+	}
 
 }
