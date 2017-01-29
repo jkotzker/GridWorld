81e1dc6be246b7ec1e6ac01d603bbc1be809188c
Joseph Kotzker <joeykotzker@gmail.com>
2017-01-29 17:38:08 -0500

    Exceptions, GridSquare, Grid
    * Implemented most of GridSquare class
    * Cleaned up Grid class
    * Added Exceptions in new package

--- src/com/gridworld/grid/Grid.java
+++ src/com/gridworld/grid/Grid.java
@@ -6,27 +6,27 @@ Initialized Array of grid_squares that includes cost dictionaries
  */
 package com.gridworld.grid;
 
-import java.util.*;
+import com.gridworld.*;
 
 public class Grid {
-	GridSquare[][] GridSquares = new GridSquare[160][120];
+	
+	GridSquare[][] GridSquares;
 	Object[][] white  = {{"horiz","vert","diagonal"},{0.5, 0.5, Math.pow(2, 0.5)/2}};
 	Object[][] lightGray  = {{"horiz","vert","diagonal"},{1, 1, Math.pow(2, 0.5)}};
 	
-	Grid(){
+	public Grid(){
 		this.GridSquares = new GridSquare[160][120];
 		for(int i = 0; i<160; i++){
 			for(int j = 0; j<120; j++){
-				GridSquare[i][j]= GridSquare(i,j,color.white);
+				GridSquares[i][j]= new GridSquare(i,j,Color.WHITE);
 			}
+		}
 	}
 	
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
 		return this.GridSquares.toString();
-	}
-		
 	}
 	
 }
--- src/com/gridworld/grid/GridSquare.java
+++ src/com/gridworld/grid/GridSquare.java
@@ -1,23 +1,104 @@
 package com.gridworld.grid;
 
+import com.gridworld.exceptions.CoordinateException;
+import com.gridworld.exceptions.HighwayException;
 import com.gridworld.grid.Color;
 
+/**
+ * GridSquare Class
+ * @author josephkotzker
+ *
+ * Class representing a single grid in a map of terrain (represented by the Grid class).
+ */
 public class GridSquare {
 	
-	Coordinates coordinates = new Coordinates(0,0);
+	// Coordinates object to hold only allowed X and Y coordinate values
+	Coordinates coordinates;
 	
+	// Boolean variables tracking highway membership
 	Boolean memberOfVerticalHighway = false;
 	Boolean memberOfHorizontalHighway = false;
 	
+	// Color Enum
 	Color color;
 	
+	/**
+	 * Constructor for the GridSquare class
+	 * 
+	 * @param x			the x vale
+	 * @param y			the y value
+	 * @param color 	the (initial) color
+	 */
 	public GridSquare(int x, int y, Color color){
 		
-		this.coordinates.setX(x);
-		this.coordinates.setY(y);
-		
+		try {
+			this.coordinates = new Coordinates(x,y);
+		} catch (CoordinateException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+
 		this.color = color;
 		
 	}
-
+	
+	/**
+	 * Compute cost of traversal from this to target
+	 * @param target 	the target GridSquare
+	 * @return double 	the cost of traversal as a double 
+	 */
+	public double computeCost(GridSquare target){
+		
+		double cost = 0.0;
+		//TODO: Compute cost of traversal from this to target
+		return cost;
+		
+	}
+	
+	/**
+	 * Change GridSquare's color
+	 * @param newcol 	the new Color for the GridSquare
+	 */
+	public void setColor(Color newcol){
+		this.color = newcol;
+		return;
+	}
+	
+	/**
+	 * Set the GridSquare as a member of a vertical highway
+	 * @throws HighwayException 	Exception containing error message regarding failure to set as highway member
+	 */
+	public void setMemberOfVerticalHighway() throws HighwayException{
+		
+		if (this.memberOfVerticalHighway == false && this.memberOfHorizontalHighway == false){
+			this.memberOfVerticalHighway = true;
+			return;
+		} else if (this.memberOfVerticalHighway == true){
+			throw new HighwayException("Highway Error: Already a member of a vertical highway, duplicates not allowed.");
+		} else if(this.memberOfHorizontalHighway == true) {
+			throw new HighwayException("Highway Error: Already a member of a horizontal highway, intersections not allowed.");
+		} else {
+			throw new HighwayException("Highway Error: Unknown error, this should never happen.");
+		}
+	}
+	
+	/**
+	 * Set the GridSquare as a member of a vertical highway
+	 * @throws HighwayException 	Exception containing error message regarding failure to set as highway member
+	 */
+	public void setMemberOfHorizontalHighway() throws HighwayException{
+		
+		if (this.memberOfVerticalHighway == false && this.memberOfHorizontalHighway == false){
+			this.memberOfHorizontalHighway = true;
+			return;
+		} else if (this.memberOfHorizontalHighway == true){
+			throw new HighwayException("Highway Error: Already a member of a horizontal highway, duplicates not allowed.");
+		} else if(this.memberOfVerticalHighway == true) {
+			throw new HighwayException("Highway Error: Already a member of a vertical highway, intersections not allowed.");
+		} else {
+			throw new HighwayException("Highway Error: Unknown error, this should never happen.");
+		}
+	}
+	
+	
 }
