d8ecc48ecc886f7e951351a0f303655cb04b5691
Esther <eshiman@gmail.com>
2017-02-01 01:50:32 -0500

    I ADDED ALL THE THINGS.
    I followed all the steps to create the grid including highways, blocked
    cells and start and end cells. I haven't tested this yet.

--- src/com/gridworld/grid/Grid.java
+++ src/com/gridworld/grid/Grid.java
@@ -6,21 +6,257 @@ Initialized Array of grid_squares that includes cost dictionaries
  */
 package com.gridworld.grid;
 
-import java.util.*;
-
-import com.gridworld.grid.GridSquare;
+import java.util.HashMap;
+import java.util.Random;
+import java.util.Stack;
+import com.gridworld.*;
 
 public class Grid {
-	GridSquare[][] GridSquares = new GridSquare[160][120];
-	Object[][] white  = {{"horiz","vert","diagonal"},{0.5, 0.5, Math.pow(2, 0.5)/2}};
-	Object[][] lightGray  = {{"horiz","vert","diagonal"},{1, 1, Math.pow(2, 0.5)}};
-	
-	Grid(){
+
+	GridSquare[][] GridSquares;
+	HashMap<String, Double> whiteCosts = new HashMap<String, Double>();
+	HashMap<String, Double> lightgrayCosts = new HashMap<String, Double>();
+	int oneHundredCells = 0;
+	Coordinates sStart = new Coordinates(-1, -1);
+	Coordinates sGoal = new Coordinates(-1, -1);
+
+	public Grid() {
+
+		whiteCosts.put("horiz", 0.5);
+		whiteCosts.put("vert", 0.5);
+		whiteCosts.put("diagonal", Math.pow(2, 0.5) / 2);
+
+		lightgrayCosts.put("horiz", 1.0);
+		lightgrayCosts.put("vert", 1.0);
+		lightgrayCosts.put("diagonal", Math.pow(2, 0.5));
+
 		this.GridSquares = new GridSquare[160][120];
-		for(int i = 0; i<160; i++){
-			for(int j = 0; j<120; j++){
-				this.GridSquares[i][j]= GridSquare(i,j,color.white);
+		for (int i = 0; i < 160; i++) {
+			for (int j = 0; j < 120; j++) {
+				GridSquares[i][j] = new GridSquare(i, j, Color.WHITE);
 			}
+		}
+
+		// Step 2: Placement of Hard Cells
+		// Choose 8 random coordinates
+		for (int n = 0; n < 8; n++) {
+			int x = randomNumberGenerator(0, 119);
+			int y = randomNumberGenerator(0, 159);
+			// For each coordinate, set 31x31 blocks 50% chance light gray
+			for (int i = inRange(x - 15, 119); i < inRange(x + 15, 119); i++) {
+				for (int j = inRange(y - 15, 159); j < inRange(y + 15, 159); j++) {
+					if (percentChance(50)) {
+						this.GridSquares[i][j].setColor(Color.LIGHT_GRAY);
+					}
+				}
+			}
+
+		}
+
+		// Step 3: Set up Highway
+		Stack<Coordinates> highwayStack = new Stack<Coordinates>();
+		Coordinates highwayStart = new Coordinates(-1, -1);
+		int Counter = 0;
+		while (highwayStack.size() < 5) {
+			Counter++;
+			if (Counter > 25) {
+				clearHighways();
+				highwayStack.clear();
+				Counter = 0;
+			}
+			int x = randomNumberGenerator(0, 119);
+			int y = randomNumberGenerator(0, 159);
+			if (percentChance(50)) {
+				y = randomNumberGenerator(0, 1) * 159;
+				highwayStart = new Coordinates(x, y);
+				y = markHoriz(x, y, 20, true, 0);
+			} else {
+				x = randomNumberGenerator(0, 1) * 119;
+				highwayStart = new Coordinates(x, y);
+				x = markVert(x, y, 20, true, 0);
+			}
+			highwayStack.push(highwayStart);
+			if (x == -1 || y == -1) {
+				highwayStack.pop();
+				x = -2;
+			}
+			while (x != -2 || y != -2) {
+				if (percentChance(60)) {
+					if (highwayStack.peek().XVal == x) {
+						y = markHoriz(x, y, 20, true, 0);
+					} else {
+						x = markVert(x, y, 20, true, 0);
+					}
+				} else {
+					if (percentChance(50)) {
+						if (highwayStack.peek().XVal == x) {
+							y = markVert(x, y, 20, true, 0);
+						} else {
+							x = markHoriz(x, y, 20, true, 0);
+						}
+					} else {
+						if (highwayStack.peek().XVal == x) {
+							y = markVert(x, y, -20, true, 0);
+						} else {
+							x = markHoriz(x, y, -20, true, 0);
+						}
+					}
+				}
+				if (x == -1 || y == -1) {
+					highwayStack.pop();
+				}
+			}
+		}
+
+		// Step 4: Select 20% cells to be blocked
+		int i = 0;
+		while (i < 0.20 * 3850) {
+			int x = randomNumberGenerator(0, 119);
+			int y = randomNumberGenerator(0, 159);
+			if (!this.GridSquares[x][y].memberOfHorizontalHighway
+					&& !this.GridSquares[x][y].memberOfHorizontalHighway) {
+				this.GridSquares[x][y].setColor(Color.DARK_GRAY);
+				i++;
+			}
+		}
+
+		// Step 5: Select sStart and sGoal
+		do{
+		this.sStart = generateStartOrGoal();
+		this.sGoal = generateStartOrGoal();
+		distance = math.pow((this.sStart.XVal-this.sGoal.XVal),2)+ math.pow((this.sStart.YVal-this.sGoal.YVal,2));
+		} while(distance)
+
+	}
+
+
+
+	private int randomNumberGenerator(int min, int max) {
+		Random rand = new Random();
+		return rand.nextInt(max) + min;
+	}
+
+	private int inRange(int val, int max) {
+		if (val < 0) {
+			return 0;
+		}
+		if (val > max) {
+			return max;
+		}
+		return val;
+	}
+
+	private boolean percentChance(int percent) {
+		if (randomNumberGenerator(0, 100) < percent) {
+			return true;
+		}
+		return false;
+	}
+
+	/*
+	 * Creates horizontal highways. author: Esther Shimanovich
+	 */
+	private int markHoriz(int row, int col, int blocksToTraverse, boolean markHighway, int direction) {
+		// Direction -1 means LEFT, +1 means RIGHT, 0 means BORDER
+		// If we are on highway already, and we wanted to build, then exit
+		if ((this.GridSquares[row][col].memberOfHorizontalHighway || this.GridSquares[row][col].memberOfVerticalHighway)
+				&& markHighway == true) {
+			return -1;
+		}
+		// If on border, set direction correctly
+		if (col == 0 || col == 159) {
+			direction = 2 * col / 159 - 1;
+		}
+		// Now we traverse
+		int n = 0;
+		while (n < blocksToTraverse) {
+			this.GridSquares[row][col + n * direction].memberOfHorizontalHighway = markHighway;
+			oneHundredCounter(markHighway);
+			// Restart path if Border or Intersection on next block
+			int nextCol = inRange(col + (n + 1) * direction, 159);
+			boolean hitIntersection = (this.GridSquares[row][nextCol].memberOfHorizontalHighway == true
+					|| this.GridSquares[row][nextCol].memberOfVerticalHighway == true) && markHighway == true;
+			boolean hitBorder = nextCol != col + (n + 1) * direction;
+			if ((hitBorder && this.oneHundredCells < 100) || hitIntersection) {
+				markHoriz(row, col + n * direction, n, false, -1 * direction);
+				return -1;
+			}
+			if (hitBorder && this.oneHundredCells >= 100) {
+				return -2;
+			}
+			// Otherwise, continue traversing
+			n++;
+		}
+		return col + blocksToTraverse * direction;
+	}
+
+	/*
+	 * Creates vertical highways. author: Esther Shimanovich
+	 */
+	private int markVert(int row, int col, int blocksToTraverse, boolean markHighway, int direction) {
+		// Direction -1 means DOWN, +1 means UP, 0 means BORDER
+		// If we are on highway already, and we wanted to build, then exit
+		if ((this.GridSquares[row][col].memberOfHorizontalHighway || this.GridSquares[row][col].memberOfVerticalHighway)
+				&& markHighway == true) {
+			return -1;
+		}
+		// If on border, set direction correctly
+		if (row == 0 || row == 119) {
+			direction = 2 * col / 119 - 1;
+		}
+		// Now we traverse
+		int n = 0;
+		while (n < blocksToTraverse) {
+			this.GridSquares[row + n * direction][col].memberOfVerticalHighway = markHighway;
+			oneHundredCounter(markHighway);
+			// Restart path if Border or Intersection on next block
+			int nextRow = inRange(row + (n + 1) * direction, 119);
+			boolean hitIntersection = (this.GridSquares[nextRow][col].memberOfHorizontalHighway == true
+					|| this.GridSquares[nextRow][col].memberOfVerticalHighway == true) && markHighway == true;
+			boolean hitBorder = nextRow != row + (n + 1) * direction;
+			if ((hitBorder && this.oneHundredCells < 100) || hitIntersection) {
+				markVert(row + n * direction, col, n, false, -1 * direction);
+				return -1;
+			}
+			if (hitBorder && this.oneHundredCells >= 100) {
+				return -2;
+			}
+			// Otherwise, continue traversing
+			n++;
+		}
+		return row + blocksToTraverse * direction;
+	}
+
+	private void oneHundredCounter(boolean changeCounter) {
+		if (changeCounter == true) {
+			this.oneHundredCells++;
+			return;
+		}
+		this.oneHundredCells--;
+		return;
+	}
+
+	private void clearHighways() {
+		for (int i = 0; i < 119; i++) {
+			for (int j = 0; j < 119; j++) {
+				this.GridSquares[i][j].memberOfHorizontalHighway = false;
+				this.GridSquares[i][j].memberOfVerticalHighway = false;
+			}
+		}
+	}
+
+	private Coordinates generateStartOrGoal() {
+		int x = -1;
+		int y = -1;
+		do {
+			y = randomNumberGenerator(0, 159);
+			if (percentChance(50)) {
+				x = randomNumberGenerator(0, 19);
+			} else {
+				x = randomNumberGenerator(99, 119);
+			}
+		} while (this.GridSquares[x][y].color.equals(Color.DARK_GRAY));
+		return new Coordinates(x, y);
 	}
 	
 	@Override
@@ -28,7 +264,5 @@ public class Grid {
 		// TODO Auto-generated method stub
 		return this.GridSquares.toString();
 	}
-		
-	}
-	
+
 }
--- src/com/gridworld/grid/GridSquare.java
+++ src/com/gridworld/grid/GridSquare.java
@@ -1,23 +1,183 @@
 package com.gridworld.grid;
 
+import com.gridworld.app.Main;
+import com.gridworld.exceptions.CoordinateException;
+import com.gridworld.exceptions.HighwayException;
+import com.gridworld.exceptions.TraversalException;
 import com.gridworld.grid.Color;
 
+/**
+ * GridSquare Class
+ * @author josephkotzker
+ *
+ * Class representing a single grid in a map of terrain (represented by the Grid class).
+ */
 public class GridSquare {
 	
-	Coordinates coordinates = new Coordinates(0,0);
+	// Coordinates object to hold only allowed X and Y coordinate values
+	Coordinates coordinates;
 	
+	// Boolean variables tracking highway membership
 	Boolean memberOfVerticalHighway = false;
 	Boolean memberOfHorizontalHighway = false;
 	
+	// Color Enum
 	Color color;
 	
+	/**
+	 * Constructor for the GridSquare class
+	 * 
+	 * @param x			the x vale
+	 * @param y			the y value
+	 * @param color 	the (initial) color
+	 */
 	public GridSquare(int x, int y, Color color){
 		
-		this.coordinates.setX(x);
-		this.coordinates.setY(y);
-		
+		try {
+			this.coordinates = new Coordinates(x,y);
+		} catch (CoordinateException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+
 		this.color = color;
 		
 	}
-
+	
+	/**
+	 * Compute cost of traversal from this to target
+	 * @param target 	the target GridSquare
+	 * @return double 	the cost of traversal as a double 
+	 * @throws TraversalException 
+	 */
+	public double computeCost(GridSquare target) throws TraversalException{
+		
+		double cost = 0.0;
+		int targetX = target.coordinates.XVal;
+		int targetY = target.coordinates.YVal;
+		
+		int diffX = targetX - this.coordinates.XVal;
+		int diffY = targetY - this.coordinates.YVal;
+		Color targetColor = target.color;
+		
+		if (this.color ==  Color.WHITE && targetColor == Color.WHITE){
+			
+			if(diffX == 0){
+				// Vertical Move
+				cost = Main.grid.whiteCosts.get("vert")+Main.grid.whiteCosts.get("vert");
+				if(this.memberOfVerticalHighway && target.memberOfVerticalHighway) 
+					cost = cost*0.25;
+				return cost;
+			} else if (diffY == 0) {
+				// Horizontal Move
+				cost = Main.grid.whiteCosts.get("horiz")+Main.grid.whiteCosts.get("horiz");
+				if(this.memberOfHorizontalHighway && target.memberOfHorizontalHighway) 
+					cost = cost*0.25;
+				return cost;
+			} else if (diffX == 1 || diffX == -1 || diffY == 1 || diffY == -1){
+				// Diagonal Move
+				cost = Main.grid.whiteCosts.get("diagonal")+Main.grid.whiteCosts.get("diagonal");
+				return cost;
+			} else {
+				throw new TraversalException("Cost computation failed, traversal is invalid.");
+			}
+			
+		} else if (this.color ==  Color.LIGHT_GRAY && targetColor == Color.LIGHT_GRAY) {
+			
+			if(diffX == 0){
+				// Vertical Move
+				cost = Main.grid.lightgrayCosts.get("vert")+Main.grid.lightgrayCosts.get("vert");
+				if(this.memberOfVerticalHighway && target.memberOfVerticalHighway) 
+					cost = cost*0.25;
+				return cost;
+			} else if (diffY == 0) {
+				// Horizontal Move
+				cost = Main.grid.lightgrayCosts.get("horiz")+Main.grid.lightgrayCosts.get("horiz");
+				if(this.memberOfHorizontalHighway && target.memberOfHorizontalHighway) 
+					cost = cost*0.25;
+				return cost;
+			} else if (diffX == 1 || diffX == -1 || diffY == 1 || diffY == -1){
+				// Diagonal Move
+				cost = Main.grid.lightgrayCosts.get("diagonal")+Main.grid.lightgrayCosts.get("diagonal");
+				return cost;
+			} else {
+				throw new TraversalException("Cost computation failed, traversal is invalid.");
+			}
+			
+		} else if ((this.color ==  Color.LIGHT_GRAY && targetColor == Color.WHITE) || (this.color ==  Color.WHITE && targetColor == Color.LIGHT_GRAY)) {
+			
+			if(diffX == 0){
+				// Vertical Move
+				cost = Main.grid.lightgrayCosts.get("vert")+Main.grid.whiteCosts.get("vert");
+				if(this.memberOfVerticalHighway && target.memberOfVerticalHighway) 
+					cost = cost*0.25;
+				return cost;
+			} else if (diffY == 0) {
+				// Horizontal Move
+				cost = Main.grid.lightgrayCosts.get("horiz")+Main.grid.whiteCosts.get("horiz");
+				if(this.memberOfHorizontalHighway && target.memberOfHorizontalHighway) 
+					cost = cost*0.25;
+				return cost;
+			} else if (diffX == 1 || diffX == -1 || diffY == 1 || diffY == -1){
+				// Diagonal Move
+				cost = Main.grid.lightgrayCosts.get("diagonal")+Main.grid.whiteCosts.get("diagonal");
+				return cost;
+			} else {
+				throw new TraversalException("Cost computation failed, traversal is invalid.");
+			}
+			
+		} else if (this.color == Color.DARK_GRAY || targetColor == Color.DARK_GRAY) {
+			throw new TraversalException("Cannot traverse to or from a blocked square, traversal is invalid.");
+		} else {
+			throw new TraversalException("Cost computation failed, traversal is invalid.");
+		}
+		
+	}
+	
+	/**
+	 * Change GridSquare's color
+	 * @param newcol 	the new Color for the GridSquare
+	 */
+	public void setColor(Color newcol){
+		this.color = newcol;
+		return;
+	}
+	
+	/**
+	 * Set the GridSquare as a member of a vertical highway
+	 * @throws HighwayException 	Exception containing error message regarding failure to set as highway member
+	 */
+	public void setMemberOfVerticalHighway() throws HighwayException{
+		
+		if (this.memberOfVerticalHighway == false && this.memberOfHorizontalHighway == false){
+			this.memberOfVerticalHighway = true;
+			return;
+		} else if (this.memberOfVerticalHighway == true){
+			throw new HighwayException("Highway Error: Already a member of a vertical highway, duplicates not allowed.");
+		} else if(this.memberOfHorizontalHighway == true) {
+			throw new HighwayException("Highway Error: Already a member of a horizontal highway, intersections not allowed.");
+		} else {
+			throw new HighwayException("Highway Error: Unknown error, this should never happen.");
+		}
+	}
+	
+	/**
+	 * Set the GridSquare as a member of a vertical highway
+	 * @throws HighwayException 	Exception containing error message regarding failure to set as highway member
+	 */
+	public void setMemberOfHorizontalHighway() throws HighwayException{
+		
+		if (this.memberOfVerticalHighway == false && this.memberOfHorizontalHighway == false){
+			this.memberOfHorizontalHighway = true;
+			return;
+		} else if (this.memberOfHorizontalHighway == true){
+			throw new HighwayException("Highway Error: Already a member of a horizontal highway, duplicates not allowed.");
+		} else if(this.memberOfVerticalHighway == true) {
+			throw new HighwayException("Highway Error: Already a member of a vertical highway, intersections not allowed.");
+		} else {
+			throw new HighwayException("Highway Error: Unknown error, this should never happen.");
+		}
+	}
+	
+	
 }
