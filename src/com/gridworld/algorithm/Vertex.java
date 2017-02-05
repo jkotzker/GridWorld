c297e84be9bc777509afe19596b7674e74ac9212
Esther <eshiman@gmail.com>
2017-02-05 16:37:25 -0500

    Additional Work on Search

--- src/com/gridworld/algorithm/Vertex.java
+++ src/com/gridworld/algorithm/Vertex.java
@@ -1,13 +1,27 @@
 package com.gridworld.algorithm;
 
 import com.gridworld.grid.*;
+import java.util.LinkedList;
+
 public class Vertex {
-	public int g = Integer.MAX_VALUE;
+	public double g = Double.POSITIVE_INFINITY;
 	public Vertex Parent = null;
-	
-	
-	
-	public Vertex(GridSquare currentBlock){
-		
+	public GridSquare gridSquare = null;
+	public LinkedList<Vertex> succ = new LinkedList<Vertex>();
+	// || this.gridSquare.color == SquareColor.DARK_GRAY
+
+	public boolean isClosed() {
+		if (this.gridSquare.isPath == true) {
+			return true;
+		}
+		return false;
+	}
+
+	public Vertex(GridSquare currentBlock, Grid currentGrid) {
+		for (int x = -1; x <= 1; x++) {
+			for (int y = -1; y <= 1; y++) {
+				succ.add(new Vertex(currentGrid.GridSquares[currentBlock.coordinates.XVal + x][currentBlock.coordinates.YVal + y], currentGrid));
+			}
+		}
 	}
 }
